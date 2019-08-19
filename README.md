17f2f63d4434d719c34c940dffe8ddc2b4618abd
Sergey Lebedev <Lebedevsd@gmail.com>
2019-08-19 22:36:56 +0200

    Updated MD file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # reader-for-reddit
+
+Idea behind that project is to use set of the libraries from android JetPack and to see how do they perform in production code.
+
+Libraries in use:
+* ViewModel
+* DataBinding
+* Coroutines
+* Dagger
+* Assisted inject
+* Retrofit
+
---
+++ app/src/main/java/com/example/readerforreddit/LoginViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.readerforreddit
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class LoginViewModel(): ViewModel(){
+    init {
+        viewModelScope.launch {
+            // Coroutine that will be canceled when the ViewModel is cleared.
+        }
+    }
+}
